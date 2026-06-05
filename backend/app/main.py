89ad488e83ac455c31a86b5914ded876b8480aa3
sagar kumar <kumarsagar77283@gmail.com>
2026-06-05 19:51:39 +0530

    Add Vercel domains to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -172,9 +172,10 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+
+    "https://fitnova-ai-9yua.vercel.app",
     "https://fitnova-ai-9yua-i7m8979vk-sagars-projects-495992fd.vercel.app",
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
