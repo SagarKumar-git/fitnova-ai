17aecde4f308f8642f3cfdad437b25335dda580f
sagar kumar <kumarsagar77283@gmail.com>
2026-06-06 12:37:36 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -174,7 +174,7 @@ origins = [
     "http://127.0.0.1:3000",
 
     "https://fitnova-ai-9yua.vercel.app",
-    "https://fitnova-ai-9yua-i7m8979vk-sagars-projects-495992fd.vercel.app",
+    "https://fitnova-ai-txvs.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
